c6c2f40e029de08fa578ab900e3741ff27f57a04
Emma Cote <emmacote@Emmas-MacBook-Pro.local>
2026-07-06 13:41:06 -0400

    The Vue playground has elements of a separate quote generator that need not be there.

--- quoter/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-full_file_path = BASE_DIR / "quotes.txt"
-
-print(f"__init__ path lives here: {full_file_path}")
-
-with open(full_file_path, "rt") as f:
-    quotearr = [line.strip() for line in f]
